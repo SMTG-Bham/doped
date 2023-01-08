d3e4eec8a8b65cbe7f21857bee41f387a427a681
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-01-08 14:01:38 +0000

    Add check if pymatgen-analysis-defects is installed (and tell the user to uninstall it) and for current pymatgen version
    Former-commit-id: 21fa08f2df76b4f3a7b738ac55653ccb5bc65b35
    Former-commit-id: e757a976431ea6e0d10190570f9840e332d36170

--- doped/__init__.py
+++ doped/__init__.py
@@ -1,8 +1,35 @@
 from datetime import date
+from importlib.metadata import version, PackageNotFoundError
 
-# print("""I'm unapologetic you can tell by what I'm on.
-# If you don't like this, fuck you, this is nothing but a song.\n""")
 if date.today().weekday() in [5, 6]:
     print("""Working on the weekend, like usual...\n""")
 if date.today().weekday() == 5:
     print("Seriously though, everyone knows Saturday's for the boys/girls...\n")
+
+def _check_pmg_compatibility():
+    try:
+        v_pmg_def = version("pymatgen-analysis-defects")
+    except PackageNotFoundError:
+        v_pmg_def = False
+
+    try:
+        v_pmg = version("pymatgen")
+    except Exception:
+        v_pmg = False
+
+    if v_pmg_def:
+        raise TypeError(
+            f"You have the `pymatgen-analysis-defects` package installed, "
+            f"which is currently incompatible with `doped`. "
+            f"Please uninstall `pymatgen-analysis-defects` (with pip uninstall pymatgen-analysis-defects) "
+            f"and try again."
+        )
+
+    if v_pmg > "2022.7.25":
+        raise TypeError(
+            f"You have the version {v_pmg} of `pymatgen` installed, which is currently "
+            f"incompatible with doped. "
+            f"Please revert this package (with `pip install pymatgen==2022.7.25`) and restart the kernel."
+        )
+_check_pmg_compatibility()
+
