21fa08f2df76b4f3a7b738ac55653ccb5bc65b35
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-01-08 14:01:38 +0000

    Add check if pymatgen-analysis-defects is installed (and tell the user to uninstall it) and for current pymatgen version

--- setup.py
+++ setup.py
@@ -22,60 +22,15 @@ def readme():
         return readme_file.read()
 
 
-setup(
-    name="doped",
-    packages=find_packages(),
-    version="0.2.0",
-    install_requires=[
-        "numpy",
-        "pymatgen<2022.8.23",
-        "matplotlib",
-        "monty>=3.0.2",
-        "tabulate",
-        "ase",
-        "shakenbreak=22.11.1",  # pymatgen<2022.8.23
-    ],
-    package_data={"doped.pycdt.utils": ["*.yaml"], "doped": ["default_POTCARs.yaml"]},
-    author="Seán Kavanagh",
-    author_email="sean.kavanagh.19@ucl.ac.uk",
-    maintainer="Seán Kavanagh",
-    maintainer_email="sean.kavanagh.19@ucl.ac.uk",
-    url="https://github.com/SMTG-UCL/doped",
-    description="Collection of Python modules & functions to perform "
-    "and process solid-state defect calculations",
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Chemistry",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    license="MIT",
-    scripts=glob.glob(os.path.join(SETUP_PTH, "scripts", "*")),
-    test_suite="nose.collector",
-    tests_require=["nose", "pytest"],
-    cmdclass={
-        "install": PostInstallCommand,
-        "develop": PostDevelopCommand,
-        "egg_info": CustomEggInfoCommand,
-    },
-)
-
-
 def pmg_analysis_defects_warning():
     """
     Print warning message if pymatgen-analysis-defects is installed.
     """
     installed_packages = str(subprocess.check_output([sys.executable, "-m", "pip", "list"]))
     if "pymatgen-analysis-defects" in installed_packages:
-        warnings.warn("pymatgen-analysis-defects is installed, which will cause incompatibility issues with doped.
-            "Please uninstall pymatgen-analysis-defects with 'pip uninstall pymatgen-analysis-defects'.")
+        print("Test!!")
+        warnings.warn("pymatgen-analysis-defects is installed, which will cause incompatibility issues with doped. "
+                      "Please uninstall pymatgen-analysis-defects with 'pip uninstall pymatgen-analysis-defects'.")
 
 
 class PostInstallCommand(install):
@@ -121,3 +76,49 @@ class CustomEggInfoCommand(egg_info):
         """
         egg_info.run(self)
         pmg_analysis_defects_warning()
+
+setup(
+    name="doped",
+    packages=find_packages(),
+    version="0.2.0",
+    install_requires=[
+        "numpy",
+        "pymatgen<2022.8.23",
+        "matplotlib",
+        "monty>=3.0.2",
+        "tabulate",
+        "ase",
+        "shakenbreak==22.11.1",  # pymatgen<2022.8.23
+    ],
+    package_data={"doped.pycdt.utils": ["*.yaml"], "doped": ["default_POTCARs.yaml"]},
+    author="Seán Kavanagh",
+    author_email="sean.kavanagh.19@ucl.ac.uk",
+    maintainer="Seán Kavanagh",
+    maintainer_email="sean.kavanagh.19@ucl.ac.uk",
+    url="https://github.com/SMTG-UCL/doped",
+    description="Collection of Python modules & functions to perform "
+    "and process solid-state defect calculations",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    license="MIT",
+    scripts=glob.glob(os.path.join(SETUP_PTH, "scripts", "*")),
+    test_suite="nose.collector",
+    tests_require=["nose", "pytest"],
+    cmdclass={
+        "install": PostInstallCommand,
+        "develop": PostDevelopCommand,
+        "egg_info": CustomEggInfoCommand,
+    },
+)
+
