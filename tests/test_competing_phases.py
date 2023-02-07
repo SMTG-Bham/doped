8486e27bf1408ea956c6db47a848b7d4397914bd
brlec <zccabrl@ucl.ac.uk>
2023-02-07 12:09:15 +0000

    add some tests for vasprun parsing
    Former-commit-id: 2ae3962631171fefa535446ed0f8c0814d22a50c

--- tests/test_competing_phases.py
+++ tests/test_competing_phases.py
@@ -12,38 +12,36 @@ class ChemPotsTestCase(unittest.TestCase):
         self.stable_system = 'ZrO2'
         self.unstable_system = 'Zr2O'
         self.extrinsic_species = 'La'
+        self.csv_path = self.path / 'zro2_competing_phase_energies.csv'
+        self.csv_path_ext = self.path / 'zro2_la_competing_phase_energies.csv'
 
         
     def test_cpa_csv(self): 
-        csv_path = self.path / 'zro2_competing_phase_energies.csv'
-        csv_path_ext = self.path / 'zro2_la_competing_phase_energies.csv'
-        self.stable_cpa = CompetingPhasesAnalyzer(self.stable_system)
-        self.stable_cpa.from_csv(csv_path)
+        stable_cpa = CompetingPhasesAnalyzer(self.stable_system)
+        stable_cpa.from_csv(self.csv_path)
         self.ext_cpa = CompetingPhasesAnalyzer(self.stable_system, self.extrinsic_species)
-        self.ext_cpa.from_csv(csv_path_ext)
+        self.ext_cpa.from_csv(self.csv_path_ext)
 
-        self.assertEqual(len(self.stable_cpa.elemental), 2)
+        self.assertEqual(len(stable_cpa.elemental), 2)
         self.assertEqual(len(self.ext_cpa.elemental), 3)
-        self.assertEqual(self.stable_cpa.data[0]['formula'], 'O2')
-        self.assertEqual(self.ext_cpa.data[-1]['formula'], 'La2O3')
-        self.assertAlmostEqual(self.ext_cpa.data[-1]['energy_per_fu'], -49.046189555)
+        self.assertEqual(stable_cpa.data[0]['formula'], 'O2')
+        self.assertEqual(self.ext_cpa.data[-1]['formula'], 'La2Zr2O7')
+        self.assertAlmostEqual(self.ext_cpa.data[-1]['energy_per_fu'], -119.619571095)
 
     # test chempots 
     def test_cpa_chempots(self): 
-        csv_path = self.path / 'zro2_competing_phase_energies.csv'
-        csv_path_ext = self.path / 'zro2_la_competing_phase_energies.csv'
-        self.stable_cpa = CompetingPhasesAnalyzer(self.stable_system)
-        self.stable_cpa.from_csv(csv_path)
-        df1 = self.stable_cpa.calculate_chempots()
+        stable_cpa = CompetingPhasesAnalyzer(self.stable_system)
+        stable_cpa.from_csv(self.csv_path)
+        df1 = stable_cpa.calculate_chempots()
         self.assertEqual(list(df1['O'])[0], 0)
 
         self.unstable_cpa = CompetingPhasesAnalyzer(self.unstable_system)
-        self.unstable_cpa.from_csv(csv_path)
+        self.unstable_cpa.from_csv(self.csv_path)
         with self.assertRaises(ValueError): 
             self.unstable_cpa.calculate_chempots()
 
         self.ext_cpa = CompetingPhasesAnalyzer(self.stable_system, self.extrinsic_species)
-        self.ext_cpa.from_csv(csv_path_ext)
+        self.ext_cpa.from_csv(self.csv_path_ext)
         df = self.ext_cpa.calculate_chempots()
         self.assertEqual(list(df['La_limiting_phase'])[0], 'La2Zr2O7')
         self.assertAlmostEqual(list(df['La'])[0], -9.46298748)
@@ -53,4 +51,24 @@ class ChemPotsTestCase(unittest.TestCase):
 
 
     # test vaspruns 
+    def test_vaspruns(self): 
+        cpa = CompetingPhasesAnalyzer(self.stable_system) 
+        path = self.path / 'ZrO'
+        cpa.from_vaspruns(path=path, folder='relax', csv_fname=self.csv_path) 
+        self.assertEqual(len(cpa.elemental), 2) 
+        self.assertEqual(cpa.data[0]['formula'], 'O2')
+
+        cpa_no = CompetingPhasesAnalyzer(self.stable_system)
+        with self.assertRaises(FileNotFoundError):
+            cpa_no.from_vaspruns(path='path')
+        
+        with self.assertRaises(ValueError): 
+            cpa_no.from_vaspruns(path=0)
+        
+        ext_cpa = CompetingPhasesAnalyzer(self.stable_system, self.extrinsic_species)
+        path2 = self.path / 'LaZrO'
+        ext_cpa.from_vaspruns(path=path2, folder='relax', csv_fname=self.csv_path_ext)
+        self.assertEqual(len(ext_cpa.elemental), 3)
+        self.assertEqual(ext_cpa.data[1]['formula'], 'Zr')
+
 
