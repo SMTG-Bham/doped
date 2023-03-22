e819d2b1f3e27e5018b1487179f0033282928623
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-03-22 13:46:42 +0000

    Small updates from edge case testing
    Former-commit-id: 06e7a4de9581e9987914b05d171000730c7fe662
    Former-commit-id: a7600bffb66dd7715ba4fafd2968f308c5112252

--- doped/__init__.py
+++ doped/__init__.py
@@ -1,3 +1,4 @@
+import warnings
 from datetime import date
 
 # print("""I'm unapologetic you can tell by what I'm on.
@@ -6,3 +7,6 @@ if date.today().weekday() in [5, 6]:
     print("""Working on the weekend, like usual...\n""")
 if date.today().weekday() == 5:
     print("Seriously though, everyone knows Saturday's for the boys/girls...\n")
+
+# until updated from pymatgen==2022.7.25 :
+warnings.filterwarnings("ignore", message="Using `tqdm.autonotebook.tqdm` in notebook mode")
