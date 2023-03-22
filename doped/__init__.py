d0a680558928ce58f1e2593797c48c783fe3876a
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-03-22 21:10:59 +0000

    Merge and formatting

--- doped/__init__.py
+++ doped/__init__.py
@@ -8,6 +8,7 @@ if date.today().weekday() in [5, 6]:
 if date.today().weekday() == 5:
     print("Seriously though, everyone knows Saturday's for the boys/girls...\n")
 
+
 def _check_pmg_compatibility():
     try:
         v_pmg_def = version("pymatgen-analysis-defects")
@@ -32,6 +33,8 @@ def _check_pmg_compatibility():
             f"incompatible with `doped`. Please revert this package (with `pip install "
             f"pymatgen==2022.7.25`) and restart the kernel."
         )
+
+
 _check_pmg_compatibility()
 
 # globally ignore these POTCAR warnings
@@ -40,4 +43,6 @@ warnings.filterwarnings("ignore", message="No POTCAR file with matching TITEL fi
 warnings.filterwarnings("ignore", message="Ignoring unknown variable type")
 
 # until updated from pymatgen==2022.7.25 :
-warnings.filterwarnings("ignore", message="Using `tqdm.autonotebook.tqdm` in notebook mode")
+warnings.filterwarnings(
+    "ignore", message="Using `tqdm.autonotebook.tqdm` in notebook mode"
+)
