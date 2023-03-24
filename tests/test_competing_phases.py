2ce5e01d9890a81886b920eb59c8f9e8205c6430
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-03-24 13:40:17 +0000

    Update tests (make more robust so not depending on list/file ordering etc)

--- tests/test_competing_phases.py
+++ tests/test_competing_phases.py
@@ -1,108 +1,125 @@
 import os
 from pathlib import Path
 import shutil
-import numpy as np
 import unittest
 from unittest.mock import patch
-from doped.competing_phases import CompetingPhases, CompetingPhasesAnalyzer, AdditionalCompetingPhases, _make_molecule_in_a_box, _calculate_formation_energies, combine_extrinsic
+from doped import competing_phases
 from pymatgen.core.structure import Structure
 from monty.serialization import loadfn, dumpfn
 
-class ChemPotsTestCase(unittest.TestCase): 
-    def setUp(self): 
-        self.path = Path(__file__).parents[1].joinpath('examples/competing_phases')
-        self.stable_system = 'ZrO2'
-        self.unstable_system = 'Zr2O'
-        self.extrinsic_species = 'La'
-        self.csv_path = self.path / 'zro2_competing_phase_energies.csv'
-        self.csv_path_ext = self.path / 'zro2_la_competing_phase_energies.csv'
+
+class ChemPotsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.path = Path(__file__).parents[1].joinpath("examples/competing_phases")
+        self.stable_system = "ZrO2"
+        self.unstable_system = "Zr2O"
+        self.extrinsic_species = "La"
+        self.csv_path = self.path / "zro2_competing_phase_energies.csv"
+        self.csv_path_ext = self.path / "zro2_la_competing_phase_energies.csv"
 
     def tearDown(self) -> None:
-        if Path('chempot_limits.csv').is_file(): 
-            os.remove('chempot_limits.csv')
+        if Path("chempot_limits.csv").is_file():
+            os.remove("chempot_limits.csv")
 
-        if Path('competing_phases.csv').is_file(): 
-            os.remove('competing_phases.csv')
-        
-        if Path('input.dat').is_file(): 
-            os.remove('input.dat')
-        
+        if Path("competing_phases.csv").is_file():
+            os.remove("competing_phases.csv")
+
+        if Path("input.dat").is_file():
+            os.remove("input.dat")
+
         return super().tearDown()
-    
-    def test_cpa_csv(self): 
-        stable_cpa = CompetingPhasesAnalyzer(self.stable_system)
+
+    def test_cpa_csv(self):
+        stable_cpa = competing_phases.CompetingPhasesAnalyzer(self.stable_system)
         stable_cpa.from_csv(self.csv_path)
-        self.ext_cpa = CompetingPhasesAnalyzer(self.stable_system, self.extrinsic_species)
+        self.ext_cpa = competing_phases.CompetingPhasesAnalyzer(
+            self.stable_system, self.extrinsic_species
+        )
         self.ext_cpa.from_csv(self.csv_path_ext)
 
         self.assertEqual(len(stable_cpa.elemental), 2)
         self.assertEqual(len(self.ext_cpa.elemental), 3)
-        self.assertEqual(stable_cpa.data[0]['formula'], 'O2')
-        self.assertEqual(self.ext_cpa.data[-1]['formula'], 'La2Zr2O7')
-        self.assertAlmostEqual(self.ext_cpa.data[-1]['energy_per_fu'], -119.619571095)
+        self.assertTrue(any(entry["formula"] == "O2" for entry in stable_cpa.data))
+        self.assertAlmostEqual(
+            [
+                entry["energy_per_fu"]
+                for entry in self.ext_cpa.data
+                if entry["formula"] == "La2Zr2O7"
+            ][0],
+            -119.619571095,
+        )
 
-    # test chempots 
-    def test_cpa_chempots(self): 
-        stable_cpa = CompetingPhasesAnalyzer(self.stable_system)
+    # test chempots
+    def test_cpa_chempots(self):
+        stable_cpa = competing_phases.CompetingPhasesAnalyzer(self.stable_system)
         stable_cpa.from_csv(self.csv_path)
         df1 = stable_cpa.calculate_chempots()
-        self.assertEqual(list(df1['O'])[0], 0)
-        #check if it's no longer Element
-        self.assertEqual(type(list(stable_cpa.intrinsic_chem_limits['elemental_refs'].keys())[0]), str)
+        self.assertEqual(list(df1["O"])[0], 0)
+        # check if it's no longer Element
+        self.assertEqual(
+            type(list(stable_cpa.intrinsic_chem_limits["elemental_refs"].keys())[0]),
+            str,
+        )
 
-        self.unstable_cpa = CompetingPhasesAnalyzer(self.unstable_system)
+        self.unstable_cpa = competing_phases.CompetingPhasesAnalyzer(
+            self.unstable_system
+        )
         self.unstable_cpa.from_csv(self.csv_path)
-        with self.assertRaises(ValueError): 
+        with self.assertRaises(ValueError):
             self.unstable_cpa.calculate_chempots()
 
-        self.ext_cpa = CompetingPhasesAnalyzer(self.stable_system, self.extrinsic_species)
+        self.ext_cpa = competing_phases.CompetingPhasesAnalyzer(
+            self.stable_system, self.extrinsic_species
+        )
         self.ext_cpa.from_csv(self.csv_path_ext)
         df = self.ext_cpa.calculate_chempots()
-        self.assertEqual(list(df['La_limiting_phase'])[0], 'La2Zr2O7')
-        self.assertAlmostEqual(list(df['La'])[0], -9.46298748)
+        self.assertEqual(list(df["La_limiting_phase"])[0], "La2Zr2O7")
+        self.assertAlmostEqual(list(df["La"])[0], -9.46298748)
 
-    # test vaspruns 
-    def test_vaspruns(self): 
-        cpa = CompetingPhasesAnalyzer(self.stable_system) 
-        path = self.path / 'ZrO'
-        cpa.from_vaspruns(path=path, folder='relax', csv_fname=self.csv_path) 
-        self.assertEqual(len(cpa.elemental), 2) 
-        self.assertEqual(cpa.data[0]['formula'], 'O2')
+    # test vaspruns
+    def test_vaspruns(self):
+        cpa = competing_phases.CompetingPhasesAnalyzer(self.stable_system)
+        path = self.path / "ZrO"
+        cpa.from_vaspruns(path=path, folder="relax", csv_fname=self.csv_path)
+        self.assertEqual(len(cpa.elemental), 2)
+        self.assertEqual(cpa.data[0]["formula"], "O2")
 
-        cpa_no = CompetingPhasesAnalyzer(self.stable_system)
+        cpa_no = competing_phases.CompetingPhasesAnalyzer(self.stable_system)
         with self.assertRaises(FileNotFoundError):
-            cpa_no.from_vaspruns(path='path')
-        
-        with self.assertRaises(ValueError): 
+            cpa_no.from_vaspruns(path="path")
+
+        with self.assertRaises(ValueError):
             cpa_no.from_vaspruns(path=0)
-        
-        ext_cpa = CompetingPhasesAnalyzer(self.stable_system, self.extrinsic_species)
-        path2 = self.path / 'LaZrO'
-        ext_cpa.from_vaspruns(path=path2, folder='relax', csv_fname=self.csv_path_ext)
+
+        ext_cpa = competing_phases.CompetingPhasesAnalyzer(
+            self.stable_system, self.extrinsic_species
+        )
+        path2 = self.path / "LaZrO"
+        ext_cpa.from_vaspruns(path=path2, folder="relax", csv_fname=self.csv_path_ext)
         self.assertEqual(len(ext_cpa.elemental), 3)
         self.assertEqual(ext_cpa.data[1]['formula'], 'Zr')
 
         # check if it works from a list
         all_paths = []
         for p in path.iterdir():
-            if not p.name.startswith('.'): 
-                pp = p / 'relax' / 'vasprun.xml' 
-                ppgz = p / 'relax' / 'vasprun.xml.gz' 
+            if not p.name.startswith("."):
+                pp = p / "relax" / "vasprun.xml"
+                ppgz = p / "relax" / "vasprun.xml.gz"
                 if pp.is_file():
                     all_paths.append(pp)
-                elif ppgz.is_file(): 
-                    all_paths.append(ppgz) 
-        lst_cpa = CompetingPhasesAnalyzer(self.stable_system) 
+                elif ppgz.is_file():
+                    all_paths.append(ppgz)
+        lst_cpa = competing_phases.CompetingPhasesAnalyzer(self.stable_system)
         lst_cpa.from_vaspruns(path=all_paths)
-        self.assertEqual(len(lst_cpa.elemental), 2)   
-        self.assertEqual(len(lst_cpa.vasprun_paths), 8)   
+        self.assertEqual(len(lst_cpa.elemental), 2)
+        self.assertEqual(len(lst_cpa.vasprun_paths), 8)
 
         all_fols = []
         for p in path.iterdir():
-            if not p.name.startswith('.'): 
-                pp = p / 'relax' 
+            if not p.name.startswith("."):
+                pp = p / "relax"
                 all_fols.append(pp)
-        lst_fols_cpa = CompetingPhasesAnalyzer(self.stable_system)
+        lst_fols_cpa = competing_phases.CompetingPhasesAnalyzer(self.stable_system)
         lst_fols_cpa.from_vaspruns(path=all_fols)
         self.assertEqual(len(lst_fols_cpa.elemental), 2)
 
