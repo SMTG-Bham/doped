c61b15a2c52d2e700c22512239ac45392fd5ca28
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-03-27 11:49:40 +0100

    Format with `black`
    Former-commit-id: 6dac7505fa2775189ad127fda6fb0a3ab3485595

--- doped/pycdt/utils/tests/test_log_util.py
+++ doped/pycdt/utils/tests/test_log_util.py
@@ -15,14 +15,14 @@ from doped.pycdt.utils.log_util import initialize_logging
 class InitializeLoggingTest(unittest.TestCase):
     def test_initialize_logging(self):
         self.assertEqual(initialize_logging(), None)
-        self.assertEqual(initialize_logging(filename='this_tmp_filename.txt'), None)
-        os.remove('this_tmp_filename.txt')
-        self.assertEqual(initialize_logging(level='WARNING'), None)
-        self.assertEqual(initialize_logging(level='DEBUG'), None)
-        self.assertEqual(initialize_logging(level='NOTSET'), None)
+        self.assertEqual(initialize_logging(filename="this_tmp_filename.txt"), None)
+        os.remove("this_tmp_filename.txt")
+        self.assertEqual(initialize_logging(level="WARNING"), None)
+        self.assertEqual(initialize_logging(level="DEBUG"), None)
+        self.assertEqual(initialize_logging(level="NOTSET"), None)
 
 
 import unittest
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
