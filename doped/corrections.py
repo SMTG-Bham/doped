121d1a8826ec309a47090a90b994edb02d4c0d7a
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-05-06 18:18:51 +0100

    Refactor whittled-down and modified `doped` corrections into `corrections.py` file
    Former-commit-id: f9723650421338e8264330fbd40dc34b6a1c73e7
    Former-commit-id: dd2718d0abba53c8911b9b55d5d3fff7ad5ba081

--- doped/corrections.py
+++ doped/corrections.py
@@ -1,8 +1,11 @@
 """
-This module contains calls to finite size supercell charge corrections as implemented in
-pymatgen.analysis.defects.core
+Code to compute finite-size charge corrections for charged defects in periodic systems.
+These functions are built from a combination of useful modules from pymatgen, pycdt and AIDE (by
+Adam Jackson and Alex Ganose), alongside substantial modification, in the efforts of making an
+efficient, user-friendly package for managing and analysing defect calculations,
+with publication-quality outputs.
 
-The methods implemented are:
+The charge-correction methods implemented are:
 1) Freysoldt correction for isotropic systems. Includes:
        a) PC energy
        b) potential alignment by planar averaging.
@@ -16,14 +19,25 @@ If you use the corrections implemented in this module, cite
    Kumagai and Oba, Phys. Rev. B. 89, 195205 (2014) for anisotropic correction
 """
 
-
+import warnings
+import itertools
 import copy
+from math import erfc, exp
 
 import numpy as np
-from pymatgen.analysis.defects.corrections import FreysoldtCorrection, KumagaiCorrection
 from monty.json import MontyDecoder
+from pymatgen.analysis.defects.corrections import FreysoldtCorrection, KumagaiCorrection
 
-from doped.pycdt.utils.parse_calculations import _convert_dielectric_to_tensor
+from doped.pycdt.utils.parse_calculations import (
+    _convert_dielectric_to_tensor,
+    SingleDefectParser,
+)
+
+warnings.simplefilter("default")
+warnings.filterwarnings(
+    "ignore", message="`np.int` is a deprecated alias for the builtin `int`"
+)
+warnings.filterwarnings("ignore", message="Use get_magnetic_symmetry()")
 
 
 def _monty_decode_nested_dicts(d):
@@ -263,3 +277,273 @@ def get_correction_kumagai(
             kumagai_val,
         ]
     return kumagai_val
+
+
+def freysoldt_correction_from_paths(
+    defect_file_path,
+    bulk_file_path,
+    dielectric,
+    defect_charge,
+    plot=False,
+    filename=None,
+):
+    """
+    A function for performing the Freysoldt correction with a set of file paths.
+    If this correction is used, please reference Freysoldt's original paper.
+    doi: 10.1103/PhysRevLett.102.016402
+
+    :param defect_file_path (str): file path to defect folder of interest
+    :param bulk_file_path (str): file path to bulk folder of interest
+    :param dielectric (float or int or 3x1 matrix or 3x3 matrix):
+            ionic + static contributions to dielectric constant
+    :param defect_charge (int): charge of defect structure of interest
+    :param plot (bool): decides whether to plot electrostatic potential plots or not.
+    :param filename (str): if None, plots are not saved, if a string,
+            then the plot will be saved as '{filename}_{axis}.pdf'
+    :return:
+        Dictionary of Freysoldt Correction for defect
+    """
+    sdp = SingleDefectParser.from_paths(
+        defect_file_path, bulk_file_path, dielectric, defect_charge
+    )
+    _ = sdp.freysoldt_loader()
+    if plot:
+        print(f"{sdp.defect_entry.name}, charge = {defect_charge}")
+    correction = get_correction_freysoldt(
+        sdp.defect_entry, dielectric, plot=plot, filename=filename
+    )
+
+    return correction
+
+
+def kumagai_correction_from_paths(
+    defect_file_path,
+    bulk_file_path,
+    dielectric,
+    defect_charge,
+    plot=False,
+    filename=None,
+):
+    """
+    A function for performing the Kumagai correction with a set of file paths.
+    If this correction is used, please reference Kumagai and Oba's original paper
+    (doi: 10.1103/PhysRevB.89.195205) as well as Freysoldt's original
+    paper (doi: 10.1103/PhysRevLett.102.016402
+
+    :param defect_file_path (str): file path to defect folder of interest
+    :param bulk_file_path (str): file path to bulk folder of interest
+    :param dielectric (float or int or 3x1 matrix or 3x3 matrix):
+            ionic + static contributions to dielectric constant
+    :param defect_charge (int): charge of defect structure of interest
+    :param plot (bool): decides whether to plot electrostatic potential plots or not.
+    :param filename (str): if None, plots are not saved, if a string, then the plot will be saved as
+        '{filename}.pdf'
+    :return:
+        Dictionary of Kumagai Correction for defect
+    """
+    sdp = SingleDefectParser.from_paths(
+        defect_file_path, bulk_file_path, dielectric, defect_charge
+    )
+    _ = sdp.kumagai_loader()
+    if plot:
+        print(f"{sdp.defect_entry.name}, charge = {defect_charge}")
+    correction = get_correction_kumagai(
+        sdp.defect_entry, dielectric, plot=plot, filename=filename
+    )
+
+    return correction
+
+
+# The following functions are taken from the deprecated AIDE package developed by the dynamic duo
+# Adam Jackson and Alex Ganose (https://github.com/SMTG-UCL/aide)
+
+
+def get_murphy_image_charge_correction(
+    lattice,
+    dielectric_matrix,
+    conv=0.3,
+    factor=30,
+    verbose=False,
+):
+    """Calculates the anisotropic image charge correction by Sam Murphy in eV.
+
+    This a rewrite of the code 'madelung.pl' written by Sam Murphy (see [1]).
+    The default convergence parameter of conv = 0.3 seems to work perfectly
+    well. However, it may be worth testing convergence of defect energies with
+    respect to the factor (i.e. cut-off radius).
+
+    References:
+        [1] S. T. Murphy and N. D. H. Hine, Phys. Rev. B 87, 094111 (2013).
+
+    Args:
+        lattice (list): The defect cell lattice as a 3x3 matrix.
+        dielectric_matrix (list): The dielectric tensor as 3x3 matrix.
+        conv (float): A value between 0.1 and 0.9 which adjusts how much real
+                      space vs reciprocal space contribution there is.
+        factor: The cut-off radius, defined as a multiple of the longest cell
+            parameter.
+        verbose (bool): If True details of the correction will be printed.
+
+    Returns:
+
+        The image charge correction as {charge: correction}
+    """
+    inv_diel = np.linalg.inv(dielectric_matrix)
+    det_diel = np.linalg.det(dielectric_matrix)
+    latt = np.sqrt(np.sum(lattice**2, axis=1))
+
+    # calc real space cutoff
+    longest = max(latt)
+    r_c = factor * longest
+
+    # Estimate the number of boxes required in each direction to ensure
+    # r_c is contained (the tens are added to ensure the number of cells
+    # contains r_c). This defines the size of the supercell in which
+    # the real space section is performed, however only atoms within rc
+    # will be conunted.
+    axis = np.array([int(r_c / a + 10) for a in latt])
+
+    # Calculate supercell parallelpiped and dimensions
+    sup_latt = np.dot(np.diag(axis), lattice)
+
+    # Determine which of the lattice parameters is the largest and determine
+    # reciprocal space supercell
+    recip_axis = np.array([int(x) for x in factor * max(latt) / latt])
+    recip_volume = abs(np.dot(np.cross(lattice[0], lattice[1]), lattice[2]))
+
+    # Calculatate the reciprocal lattice vectors (need factor of 2 pi)
+    recip_latt = np.linalg.inv(lattice).T * 2 * np.pi
+
+    real_space = _get_real_space(conv, inv_diel, det_diel, r_c, axis, sup_latt)
+    reciprocal = _get_recip(
+        conv,
+        recip_axis,
+        recip_volume,
+        recip_latt,
+        dielectric_matrix,
+    )
+
+    # calculate the other terms and the final Madelung potential
+    third_term = -2 * conv / np.sqrt(np.pi * det_diel)
+    fourth_term = -3.141592654 / (recip_volume * conv**2)
+    madelung = -(real_space + reciprocal + third_term + fourth_term)
+
+    # convert to atomic units
+    conversion = 14.39942
+    real_ev = real_space * conversion / 2
+    recip_ev = reciprocal * conversion / 2
+    third_ev = third_term * conversion / 2
+    fourth_ev = fourth_term * conversion / 2
+    madelung_ev = madelung * conversion / 2
+
+    correction = {}
+    for q in range(1, 8):
+        makov = 0.5 * madelung * q**2 * conversion
+        lany = 0.65 * makov
+        correction[q] = makov
+
+    if verbose:
+        print(
+            """
+    Results                      v_M^scr    dE(q=1) /eV
+    -----------------------------------------------------
+    Real space contribution    =  {:.6f}     {:.6f}
+    Reciprocal space component =  {:.6f}     {:.6f}
+    Third term                 = {:.6f}    {:.6f}
+    Neutralising background    = {:.6f}    {:.6f}
+    -----------------------------------------------------
+    Final Madelung potential   = {:.6f}     {:.6f}
+    -----------------------------------------------------""".format(
+                real_space,
+                real_ev,
+                reciprocal,
+                recip_ev,
+                third_term,
+                third_ev,
+                fourth_term,
+                fourth_ev,
+                madelung,
+                madelung_ev,
+            )
+        )
+
+        print(
+            """
+    Here are your final corrections:
+    +--------+------------------+-----------------+
+    | Charge | Point charge /eV | Lany-Zunger /eV |
+    +--------+------------------+-----------------+"""
+        )
+        for q in range(1, 8):
+            makov = 0.5 * madelung * q**2 * conversion
+            lany = 0.65 * makov
+            correction[q] = makov
+            print("|   {}    |     {:10f}   |    {:10f}   |".format(q, makov, lany))
+        print("+--------+------------------+-----------------+")
+    return correction
+
+
+def _get_real_space(conv, inv_diel, det_diel, r_c, axis, sup_latt):
+    # Calculate real space component
+    real_space = 0.0
+    axis_ranges = [range(-a, a) for a in axis]
+
+    # Pre-compute square of cutoff distance for cheaper comparison than
+    # separation < r_c
+    r_c_sq = r_c**2
+
+    def _real_loop_function(mno):
+        # Calculate the defect's fractional position in extended supercell
+        d_super = np.array(mno, dtype=float) / axis
+        d_super_cart = np.dot(d_super, sup_latt)
+
+        # Test if the new atom coordinates fall within r_c, then solve
+        separation_sq = np.sum(np.square(d_super_cart))
+        # Take all cases within r_c except m,n,o != 0,0,0
+        if separation_sq < r_c_sq and any(mno):
+            mod = np.dot(d_super_cart, inv_diel)
+            dot_prod = np.dot(mod, d_super_cart)
+            N = np.sqrt(dot_prod)
+            contribution = 1 / np.sqrt(det_diel) * erfc(conv * N) / N
+            return contribution
+        else:
+            return 0.0
+
+    real_space = sum(
+        _real_loop_function(mno) for mno in itertools.product(*axis_ranges)
+    )
+    return real_space
+
+
+def _get_recip(
+    conv,
+    recip_axis,
+    recip_volume,
+    recip_latt,
+    dielectric_matrix,
+):
+    # convert factional motif to reciprocal space and
+    # calculate reciprocal space supercell parallelpiped
+    recip_sup_latt = np.dot(np.diag(recip_axis), recip_latt)
+
+    # Calculate reciprocal space component
+    axis_ranges = [range(-a, a) for a in recip_axis]
+
+    def _recip_loop_function(mno):
+        # Calculate the defect's fractional position in extended supercell
+        d_super = np.array(mno, dtype=float) / recip_axis
+        d_super_cart = np.dot(d_super, recip_sup_latt)
+
+        if any(mno):
+            mod = np.dot(d_super_cart, dielectric_matrix)
+            dot_prod = np.dot(mod, d_super_cart)
+            contribution = exp(-dot_prod / (4 * conv**2)) / dot_prod
+            return contribution
+        else:
+            return 0.0
+
+    reciprocal = sum(
+        _recip_loop_function(mno) for mno in itertools.product(*axis_ranges)
+    )
+    scale_factor = 4 * np.pi / recip_volume
+    return reciprocal * scale_factor
