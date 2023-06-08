daa2dd166f2fd09ac827530329a9039a494e48e3
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-06-08 21:49:29 +0100

    `pre-commit` hook formatting
    Former-commit-id: 64f6715d1325060ede0ceb97e0e0569a2cd55d7b [formerly 734862c3b0a3929783ae96d20edfbb4970f274a2]
    Former-commit-id: d8eccab3d490ead1c12c3d14db5c74d45a54977a
    Former-commit-id: 2d4608e9ecfd752094f47025482f93f8e26d0da6

--- doped/pycdt/core/tests/test_chemical_potentials.py
+++ doped/pycdt/core/tests/test_chemical_potentials.py
@@ -1,25 +1,13 @@
-import copy
-import inspect
 import os
 import unittest
 from shutil import copyfile
 
-from monty.serialization import loadfn
 from monty.tempfile import ScratchDir
-from pymatgen.analysis.defects.thermodynamics import DefectPhaseDiagram
-from pymatgen.analysis.phase_diagram import PhaseDiagram
-from pymatgen.core import Composition, Element
-from pymatgen.entries.computed_entries import ComputedEntry
+from pymatgen.core import Element
 from pymatgen.ext.matproj import MPRester
 from pymatgen.util.testing import PymatgenTest
 
-from doped.pycdt.core._chemical_potentials import (
-    ChemPotAnalyzer,
-    MPChemPotAnalyzer,
-    UserChemPotAnalyzer,
-    UserChemPotInputGenerator,
-    get_mp_chempots_from_dpd,
-)
+from doped.pycdt.core._chemical_potentials import UserChemPotAnalyzer
 
 TEST_DIR = os.path.abspath(os.path.join(__file__, "..", "..", "..", "test_files"))
 
@@ -28,9 +16,7 @@ class UserChemPotAnalyzerTest(PymatgenTest):
     def setUp(self):
         with MPRester(api_key="c2LiJRMiBeaN5iXsH") as mp:
             self.bulk_ce = mp.get_entry_by_material_id("mp-2534")
-        self.UCPA = UserChemPotAnalyzer(
-            bulk_ce=self.bulk_ce, mapi_key="c2LiJRMiBeaN5iXsH"
-        )
+        self.UCPA = UserChemPotAnalyzer(bulk_ce=self.bulk_ce, mapi_key="c2LiJRMiBeaN5iXsH")
         # SK MP Imperial email A/C API key
         self.UCPA_sub = UserChemPotAnalyzer(
             bulk_ce=self.bulk_ce, sub_species=["In"], mapi_key="c2LiJRMiBeaN5iXsH"
@@ -59,21 +45,13 @@ class UserChemPotAnalyzerTest(PymatgenTest):
             cp = self.UCPA.read_phase_diagram_and_chempots(
                 full_sub_approach=False, include_mp_entries=False
             )
-            self.assertEqual(set(["As-GaAs", "Ga-GaAs"]), set(cp["facets"].keys()))
-            self.assertEqual(
-                [-5.36, -4.29],
-                [
-                    round(cp["facets"]["As-GaAs"][Element(elt)], 2)
-                    for elt in ["As", "Ga"]
-                ],
-            )
-            self.assertEqual(
-                [-6.04, -3.61],
-                [
-                    round(cp["facets"]["Ga-GaAs"][Element(elt)], 2)
-                    for elt in ["As", "Ga"]
-                ],
-            )
+            assert {"As-GaAs", "Ga-GaAs"} == set(cp["facets"].keys())
+            assert [-5.36, -4.29] == [
+                round(cp["facets"]["As-GaAs"][Element(elt)], 2) for elt in ["As", "Ga"]
+            ]
+            assert [-6.04, -3.61] == [
+                round(cp["facets"]["Ga-GaAs"][Element(elt)], 2) for elt in ["As", "Ga"]
+            ]
 
         # followed by an case where MP needs to supplement...
         with ScratchDir("."):
@@ -92,21 +70,13 @@ class UserChemPotAnalyzerTest(PymatgenTest):
             cp = self.UCPA.read_phase_diagram_and_chempots(
                 full_sub_approach=False, include_mp_entries=True
             )
-            self.assertEqual(set(["As-GaAs", "Ga-GaAs"]), set(cp["facets"].keys()))
-            self.assertEqual(
-                [-5.36, -4.29],
-                [
-                    round(cp["facets"]["As-GaAs"][Element(elt)], 2)
-                    for elt in ["As", "Ga"]
-                ],
-            )
-            self.assertEqual(
-                [-6.62, -3.03],
-                [
-                    round(cp["facets"]["Ga-GaAs"][Element(elt)], 2)
-                    for elt in ["As", "Ga"]
-                ],
-            )
+            assert {"As-GaAs", "Ga-GaAs"} == set(cp["facets"].keys())
+            assert [-5.36, -4.29] == [
+                round(cp["facets"]["As-GaAs"][Element(elt)], 2) for elt in ["As", "Ga"]
+            ]
+            assert [-6.62, -3.03] == [
+                round(cp["facets"]["Ga-GaAs"][Element(elt)], 2) for elt in ["As", "Ga"]
+            ]
 
         # quick and dirty test for finding extrinsic defects...
         with ScratchDir("."):
@@ -123,8 +93,9 @@ class UserChemPotAnalyzerTest(PymatgenTest):
                 os.path.join("PhaseDiagram", "GaAs", "vasprun.xml"),
             )
             cp = self.UCPA_sub.read_phase_diagram_and_chempots(
-                full_sub_approach=False, include_mp_entries=True)
-            self.assertEqual({'As-GaAs-In', 'Ga-GaAs-In'}, set(cp["facets"].keys()))
+                full_sub_approach=False, include_mp_entries=True
+            )
+            assert {"As-GaAs-In", "Ga-GaAs-In"} == set(cp["facets"].keys())
 
 
 if __name__ == "__main__":
