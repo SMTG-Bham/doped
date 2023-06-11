ae51cac84db18a991defaf16d4257193ad1af9e5
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-06-11 16:58:14 +0100

    Update defect generation tests

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -14,12 +14,13 @@ from pymatgen.analysis.defects.thermo import DefectEntry
 from pymatgen.analysis.structure_matcher import StructureMatcher
 from pymatgen.core.structure import PeriodicSite, Structure
 
-from doped.generation import DefectsGenerator
+from doped.generation import DefectsGenerator, get_wyckoff_dict_from_sgn
 
 
 class DefectsGeneratorTest(unittest.TestCase):
     def setUp(self):
         self.data_dir = os.path.join(os.path.dirname(__file__), "data")
+        self.cdte_data_dir = os.path.join(self.data_dir, "CdTe")
         self.example_dir = os.path.join(os.path.dirname(__file__), "..", "examples")
         self.cdte_bulk = Structure.from_file(f"{self.example_dir}/CdTe/relaxed_primitive_POSCAR")
         self.ytos_bulk_supercell = Structure.from_file(f"{self.example_dir}/YTOS/Bulk/POSCAR")
@@ -39,20 +40,20 @@ class DefectsGeneratorTest(unittest.TestCase):
             """Vacancies    Charge States    Unit Cell Coords    \x1B[3mg\x1B[0m_site    Wyckoff
 -----------  ---------------  ------------------  --------  ---------
 v_Cd         [-2,-1,0,+1]     [0.00,0.00,0.00]    1         4a
-v_Te         [-1,0,+1,+2]     [0.75,0.75,0.75]    1         4d
+v_Te         [-1,0,+1,+2]     [0.25,0.25,0.25]    1         4c
 
 Substitutions    Charge States       Unit Cell Coords    \x1B[3mg\x1B[0m_site    Wyckoff
 ---------------  ------------------  ------------------  --------  ---------
-Cd_Te            [-1,0,+1,+2,+3,+4]  [0.75,0.75,0.75]    1         4d
+Cd_Te            [-1,0,+1,+2,+3,+4]  [0.25,0.25,0.25]    1         4c
 Te_Cd            [-4,-3,-2,-1,0,+1]  [0.00,0.00,0.00]    1         4a
 
 Interstitials    Charge States       Unit Cell Coords    \x1B[3mg\x1B[0m_site    Wyckoff
 ---------------  ------------------  ------------------  --------  ---------
-Cd_i_C3v         [-1,0,+1,+2]        [0.38,0.38,0.37]    4         16e
-Cd_i_Td_Cd2.83   [-1,0,+1,+2]        [0.25,0.25,0.25]    1         4c
+Cd_i_C3v         [-1,0,+1,+2]        [0.12,0.62,0.62]    4         16e
+Cd_i_Td_Cd2.83   [-1,0,+1,+2]        [0.75,0.75,0.75]    1         4d
 Cd_i_Td_Te2.83   [-1,0,+1,+2]        [0.50,0.50,0.50]    1         4b
-Te_i_C3v         [-1,0,+1,+2,+3,+4]  [0.38,0.38,0.37]    4         16e
-Te_i_Td_Cd2.83   [-1,0,+1,+2,+3,+4]  [0.25,0.25,0.25]    1         4c
+Te_i_C3v         [-1,0,+1,+2,+3,+4]  [0.12,0.62,0.62]    4         16e
+Te_i_Td_Cd2.83   [-1,0,+1,+2,+3,+4]  [0.75,0.75,0.75]    1         4d
 Te_i_Td_Te2.83   [-1,0,+1,+2,+3,+4]  [0.50,0.50,0.50]    1         4b
 
 \x1B[3mg\x1B[0m_site = Site Multiplicity (in Primitive Unit Cell)\n"""
@@ -69,6 +70,9 @@ Te_i_Td_Te2.83   [-1,0,+1,+2,+3,+4]  [0.50,0.50,0.50]    1         4b
         assert structure_matcher.fit(prim_struc_wout_oxi, self.cdte_bulk)
         np.testing.assert_array_equal(
             cdte_defect_gen.supercell_matrix, np.array([[-2, 2, 2], [2, -2, 2], [2, 2, -2]])
+        )
+        assert structure_matcher.fit(
+            prim_struc_wout_oxi * cdte_defect_gen.supercell_matrix, cdte_defect_gen.bulk_supercell
         )
 
         # test defects
@@ -120,19 +124,27 @@ Te_i_Td_Te2.83   [-1,0,+1,+2,+3,+4]  [0.50,0.50,0.50]    1         4b
         # test defect entry attributes
         assert cdte_defect_gen.defect_entries["Cd_i_C3v_0"].name == "Cd_i_C3v_0"
         assert cdte_defect_gen.defect_entries["Cd_i_C3v_0"].charge_state == 0
-        assert cdte_defect_gen.defect_entries["Cd_i_C3v_0"].defect_type == DefectType.Interstitial
-        assert cdte_defect_gen.defect_entries["Cd_i_C3v_0"].wyckoff_label == "16e"
+        assert cdte_defect_gen.defect_entries["Cd_i_C3v_0"].defect.defect_type == DefectType.Interstitial
+        assert cdte_defect_gen.defect_entries["Cd_i_C3v_0"].wyckoff == "16e"
         assert cdte_defect_gen.defect_entries["Cd_i_C3v_0"].defect.multiplicity == 4
-        np.testing.assert_array_equal(
+        np.testing.assert_allclose(
             cdte_defect_gen.defect_entries["Cd_i_C3v_0"].sc_defect_frac_coords,
-            np.array([0.38, 0.38, 0.37]),
+            np.array([0.3125, 0.1875, 0.1875]),
         )
-        # sc_defect_frac_coords
-        # sc_entry
+        for defect_name, defect_entry in cdte_defect_gen.defect_entries.items():
+            assert defect_entry.name == defect_name
+            assert defect_entry.charge_state == int(defect_name.split("_")[-1])
+            assert defect_entry.wyckoff  # wyckoff label is not None
+            assert defect_entry.defect
+            np.testing.assert_allclose(
+                defect_entry.sc_entry.structure.lattice.matrix,
+                cdte_defect_gen.bulk_supercell.lattice.matrix,
+            )
 
         assert cdte_defect_gen.defect_entries["v_Cd_0"].defect.name == "v_Cd"
         assert cdte_defect_gen.defect_entries["v_Cd_0"].defect.oxi_state == -2
         assert cdte_defect_gen.defect_entries["v_Cd_0"].defect.multiplicity == 1
+        assert cdte_defect_gen.defect_entries["v_Cd_0"].wyckoff == "4a"
         assert cdte_defect_gen.defect_entries["v_Cd_0"].defect.defect_type == DefectType.Vacancy
         assert (
             cdte_defect_gen.defect_entries["v_Cd_0"].defect.structure
@@ -149,19 +161,13 @@ Te_i_Td_Te2.83   [-1,0,+1,+2,+3,+4]  [0.50,0.50,0.50]    1         4b
             "Cd", [0, 0, 0], cdte_defect_gen.primitive_structure.lattice
         )
 
-        # assert each defect entry has the name and wyckoff label attributes:
-        for defect_entry in cdte_defect_gen.defect_entries.values():
-            assert defect_entry.name in defect_entry.defect.name
-            assert defect_entry.wyckoff_label in defect_entry.defect.wyckoff_label
-
-        # assert structures are same as those in generated data folders
-
         # TODO: test charge states (when charge state algorithm is implemented)
-
-        # test other structures
-        # test wyckoff generation for all space groups
+        # test other input structures
         # test as_dict etc methods
         # test saving to and loading from json (and that attributes remain)
+        # test that inputting a structure with lattice vectors >10 A doesn't generate a new supercell
+        # structure, unless it has less atoms than the input structure
+        # test supercell transformation stuff!!
 
     @patch("sys.stdout", new_callable=StringIO)
     def test_generator_tqdm(self, mock_stdout):
@@ -183,3 +189,12 @@ Te_i_Td_Te2.83   [-1,0,+1,+2,+3,+4]  [0.50,0.50,0.50]    1         4b
             warnings.simplefilter("always")
             DefectsGenerator(self.cdte_bulk)
             assert len(w) == 0
+
+
+class WyckoffTest(unittest.TestCase):
+    def test_wyckoff_dict_from_sgn(self):
+        for sgn in range(1, 230):  # TODO: Space group 230 doesn't work, will PR!
+            wyckoff_dict = get_wyckoff_dict_from_sgn(sgn)
+            assert isinstance(wyckoff_dict, dict)
+            assert all(isinstance(k, str) for k in wyckoff_dict)
+            assert all(isinstance(v, list) for v in wyckoff_dict.values())
