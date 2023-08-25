821b17ea3619afd7bff3c12dac4bce0519657e94
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-08-25 13:58:03 +0100

    Bump `CHANGELOG` before release
    Former-commit-id: 3eea3d6d79666e6b3f1db1d26e3490789e6824bf

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Seán R. Kavanagh'
 author = 'Seán R. Kavanagh'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
