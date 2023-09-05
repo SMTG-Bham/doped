4d3d9bfdf739fb3d1d36c7fa659fea84566f93ba
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-09-05 16:51:32 +0100

    Suppress locale Error if not supported on GH Actions
    Former-commit-id: 136dc4a019466c549d22f05c7e62d5c9526af4ce

--- tests/test_vasp.py
+++ tests/test_vasp.py
@@ -1,6 +1,7 @@
 """
 Tests for the `doped.vasp` module.
 """
+import contextlib
 import filecmp
 import locale
 import os
@@ -448,42 +449,43 @@ class DefectsSetTest(unittest.TestCase):
 
         Weirdly seems to be the case on some old HPCs/Windows systems.
         """
-        # Temporarily set the locale to ASCII/latin encoding (doesn't support emojis or "Γ"):
-        locale.setlocale(locale.LC_CTYPE, "en_US.US-ASCII")
+        with contextlib.suppress(locale.Error):  # not supported on GH Actions
+            # Temporarily set the locale to ASCII/latin encoding (doesn't support emojis or "Γ"):
+            locale.setlocale(locale.LC_CTYPE, "en_US.US-ASCII")
 
-        single_defect_entry = self.cdte_defect_gen["Cd_i_C3v_+2"]
-        defects_set = DefectsSet(
-            single_defect_entry,
-            user_incar_settings=self.cdte_custom_test_incar_settings,
-            user_potcar_functional=None,
-        )
-        defects_set.write_files(potcar_spec=True, vasp_gam=True, unperturbed_poscar=True)
-        locale.setlocale(locale.LC_CTYPE, self.original_locale)  # should be UTF-8
+            single_defect_entry = self.cdte_defect_gen["Cd_i_C3v_+2"]
+            defects_set = DefectsSet(
+                single_defect_entry,
+                user_incar_settings=self.cdte_custom_test_incar_settings,
+                user_potcar_functional=None,
+            )
+            defects_set.write_files(potcar_spec=True, vasp_gam=True, unperturbed_poscar=True)
+            locale.setlocale(locale.LC_CTYPE, self.original_locale)  # should be UTF-8
 
-        # assert that the same folders in self.cdte_data_dir are present in the current directory
-        self.check_generated_vasp_inputs(  # tests vasp_gam
-            generated_dir="Cd_i_C3v_+2",
-            data_dir=f"{self.cdte_data_dir}/Cd_i_C3v_+2",
-            check_potcar_spec=True,
-            single_defect_dir=True,
-        )
-        self.check_generated_vasp_inputs(  # vasp_std
-            generated_dir="Cd_i_C3v_+2",
-            data_dir=f"{self.cdte_data_dir}/Cd_i_C3v_+2",
-            vasp_type="vasp_std",
-            check_poscar=True,
-            single_defect_dir=True,
-        )
-        self.check_generated_vasp_inputs(  # vasp_ncl
-            generated_dir="Cd_i_C3v_+2",
-            data_dir=f"{self.cdte_data_dir}/Cd_i_C3v_+2",
-            vasp_type="vasp_ncl",
-            check_poscar=True,
-            single_defect_dir=True,
-        )
+            # assert that the same folders in self.cdte_data_dir are present in the current directory
+            self.check_generated_vasp_inputs(  # tests vasp_gam
+                generated_dir="Cd_i_C3v_+2",
+                data_dir=f"{self.cdte_data_dir}/Cd_i_C3v_+2",
+                check_potcar_spec=True,
+                single_defect_dir=True,
+            )
+            self.check_generated_vasp_inputs(  # vasp_std
+                generated_dir="Cd_i_C3v_+2",
+                data_dir=f"{self.cdte_data_dir}/Cd_i_C3v_+2",
+                vasp_type="vasp_std",
+                check_poscar=True,
+                single_defect_dir=True,
+            )
+            self.check_generated_vasp_inputs(  # vasp_ncl
+                generated_dir="Cd_i_C3v_+2",
+                data_dir=f"{self.cdte_data_dir}/Cd_i_C3v_+2",
+                vasp_type="vasp_ncl",
+                check_poscar=True,
+                single_defect_dir=True,
+            )
 
-        # assert only +2 directory written:
-        assert not os.path.exists("Cd_i_C3v_0")
+            # assert only +2 directory written:
+            assert not os.path.exists("Cd_i_C3v_0")
 
     def test_write_files_defect_entry_list(self):
         defect_entry_list = [
