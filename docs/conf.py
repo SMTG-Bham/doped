2567f706b2f20c5d51390ca67cc2ff95ad2307e8
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-09-18 09:24:07 +0100

    Add static path
    Former-commit-id: d826b271348f59f7c0e922955a919287e6b3f553 [formerly d490d5680fd3666114575ef98e104d45a49b2e5a]
    Former-commit-id: fcba27bf1ac4030a8ddad44436e3cfc4f25e7eab

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+html_static_path = ['_static']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 myst_enable_extensions = [
