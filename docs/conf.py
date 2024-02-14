10ed17dbace740aa073291d4dcaeb4a68ecbb7c8
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2024-02-14 15:58:25 +0000

    Update release workflow to only run when `setup.py` has been updated (i.e. new version number), and refactor `doped` `master` usages to `main`
    Former-commit-id: 3eb1eaf8d3de32bff490930c293cb54e9eb6615b [formerly 09d00edea04154409d36d7d0d2b10d712541f744]
    Former-commit-id: 1f0174f1d4faefffa8c29d66ed8ad8658eea222b

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ html_context = {
     "display_github": True, # Integrate GitHub
     "github_user": "SMTG-Bham", # Username
     "github_repo": "doped", # Repo name
-    "github_version": "master", # Version
+    "github_version": "main", # Version
     "conf_py_path": "/docs/", # Path in the checkout to the docs root
 }
 
