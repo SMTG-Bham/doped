0fd52e38241d8c7e1796a22e15575e6eb4527a1e
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2024-03-28 17:41:06 -0400

    Update changelog and version number prior to release
    Former-commit-id: 04cf287142b9367b29bcf2a66151a5f457b78606 [formerly a55859b35427462d03ddedba6112ce69b393c543]
    Former-commit-id: 8991c4edbfbdb0e2a4604c13bde13f6af50f9aa2

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Seán R. Kavanagh'
 author = 'Seán R. Kavanagh'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.3'
+release = '2.4.0'
 
 
 # -- General configuration ---------------------------------------------------
