ac70f291459392a4076e47a90c95db321231269a
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2024-04-10 23:23:35 -0400

    Bump change-log and version number for release
    Former-commit-id: 2147383edf3d517099c61b0406d4cf03ef4a5e88 [formerly 7dbaaaaa7721857a9ae4557b131008804174d4a9]
    Former-commit-id: 9d8b8df4511b38bf16fb7c802df77151b1e9c352

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Seán R. Kavanagh'
 author = 'Seán R. Kavanagh'
 
 # The full version, including alpha/beta/rc tags
-release = '2.4.0'
+release = '2.4.1'
 
 
 # -- General configuration ---------------------------------------------------
