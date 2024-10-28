6476425d9aee80f56d45a5872af869d29bbdebf3
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2024-10-27 21:00:33 -0400

    Update stenciling tests
    Former-commit-id: ffbd6a9d773213a31e8511ff894ef0efc380d594

--- tests/test_stenciling.py
+++ tests/test_stenciling.py
@@ -16,7 +16,8 @@ from doped.thermodynamics import DefectThermodynamics
 from doped.utils.efficiency import Composition as doped_Composition
 from doped.utils.efficiency import IStructure as doped_IStructure
 from doped.utils.efficiency import PeriodicSite as doped_PeriodicSite
-from doped.utils.stenciling import get_defect_in_supercell
+from doped.utils.stenciling import _check_min_dist, get_defect_in_supercell
+from doped.utils.supercells import min_dist
 
 Composition.__instances__ = {}
 Composition.__eq__ = doped_Composition.__eq__
@@ -67,7 +68,7 @@ class DefectStencilingTest(unittest.TestCase):
             f"{self.Se_example_dir}/Se_Intrinsic_Thermo.json.gz"
         )
         self.Se_old_new_names_dict = {"vac_1_Se": "v_Se", "Int_Se_1": "Se_i_C2"}
-        self.sm = StructureMatcher(stol=0.02, comparator=ElementComparator(), primitive_cell=False)
+        self.tight_sm = StructureMatcher(stol=0.02, comparator=ElementComparator(), primitive_cell=False)
         # TODO: Do one of the Se extrinsic substitutions/interstitials as a test too
 
     def test_Se_20_Å_supercell(self):
@@ -78,21 +79,15 @@ class DefectStencilingTest(unittest.TestCase):
         234-atom supercell was generated from
         ``DefectsGenerator(prim_Se, supercell_gen_kwargs={"min_dist":20})``.
         """
-        # we know the 20Å stenciled supercells run on Archer2 were all correct, based on energies
-        # 222-expanded also def fine for vacancies
-        # V_Se_+2 a good test case as it's hard, has two inter-chain bridging bonds
-        # (see https://doi.org/10.26434/chemrxiv-2024-91h02 SI)
-        # then V_Se_0 an easier case
+        # these supercells were explicitly tested by performing hybrid DFT relaxations from these
+        # starting points and comparing to results of unperturbed/rattled supercell relaxations of these
+        # defects directly generated (with ``DefectsGenerator``) in this 20Å supercell
+        # (for the work described in: https://doi.org/10.26434/chemrxiv-2024-91h02)
+        # these defects are good test cases as some are not so trivial; e.g. v_Se_+2 has two inter-chain
+        # bridging bonds; see https://doi.org/10.26434/chemrxiv-2024-91h02 SI.
+        Se_20A_test_supercells = [i for i in os.listdir(self.Se_example_dir) if "20Å_Stenciled" in i]
 
-        # from new check runs, look mostly fine but previous ones were a bit better? (started with lower
-        # energies between 5 meV to 120 meV (for V_Se_+1), should check this! TODO)
-        # Think this might just be due to using previous CHGCAR/WAVECAR to start? Testing!
-        # TODO: Test that the bulk supercell returned for all Se_20A cases is the same
-
-        Se_20A_test_supercells = [
-            i for i in os.listdir(self.Se_example_dir) if "20Å_Stenciled" in i
-        ]  # TODO: Change to new files and remove old ones if tested and all good
-
+        previous_bulk = None
         for old_name, defect_entry in self.Se_intrinsic_thermo.defect_entries.items():
             name = old_name
             for key, val in self.Se_old_new_names_dict.items():
@@ -100,7 +95,7 @@ class DefectStencilingTest(unittest.TestCase):
             if name in [i.split("_20Å")[0] for i in Se_20A_test_supercells]:
                 print(f"Testing {name}")
                 reference_struct = Structure.from_file(
-                    f"{self.Se_example_dir}/{name}_20Å_Checked_Stenciled_POSCAR"
+                    f"{self.Se_example_dir}/{name}_20Å_Stenciled_POSCAR"
                 )
                 with warnings.catch_warnings(record=True) as w:
                     expanded_defect_supercell, corresponding_bulk = get_defect_in_supercell(
@@ -112,9 +107,15 @@ class DefectStencilingTest(unittest.TestCase):
                     "Note that the atomic position basis of the generated defect/bulk supercell differs"
                     in str(warning.message)
                     for warning in w
-                )
+                )  # also checks no min_dist warnings
 
-                assert self.sm.fit(reference_struct, expanded_defect_supercell)
+                _check_min_dist(
+                    expanded_defect_supercell,
+                    orig_min_dist=min_dist(defect_entry.defect_supercell),
+                    warning=False,
+                )  # check min_dist, error if minimum distance has changed for some reason
+                assert self.tight_sm.fit(reference_struct, expanded_defect_supercell)
+
                 # below we also directly compare structures, but note that this may change with updates to
                 # the code (and still be fine, there may be multiple quasi-equivalent stenciling choices
                 # depending on site selection around the boundary region); so mainly just for tracking
@@ -132,24 +133,24 @@ class DefectStencilingTest(unittest.TestCase):
 
                 if_present_rm(f"{self.Se_example_dir}/{name}_gen_20Å_POSCAR")  # tests passed, can remove
 
+                if previous_bulk is not None:  # check same bulk structure output in each case here
+                    assert previous_bulk == corresponding_bulk
+
     def test_Se_222_expanded_supercell(self):
         """
         Tests stenciling from the original 13.0 x 13.0 x 14.9 Å 81-atom Se
         supercell to a 2x2x2 expansion of this cell; 26.0 x 26.0 x 29.8 Å
         648-atom supercell.
         """
-        # 222-expanded checked for vacancies and interstitials
-
-        # lower energy v_Se_0 with bond distortion in 222 cell... check which vacancy it is! (change in
-        # metastability ordering)? TODO
-        # all good, except v_Se_-1 gets ~26 meV higher energy than unperturbed (16 meV higher than bond
-        # distortion relaxation); just residual noise? What's going on with these?
-
-        # TODO: Should get bulk supercell mismatch warning for this supercell, but not for 222 supercell?
+        # these supercells were explicitly tested by performing hybrid DFT relaxations from these
+        # starting points and comparing to results of unperturbed/rattled supercell relaxations of these
+        # defects directly generated (with ``DefectsGenerator``) in this 222-expanded supercell
+        # (for the work described in: https://doi.org/10.26434/chemrxiv-2024-91h02)
         Se_222_exp_test_supercells = [
             i for i in os.listdir(self.Se_example_dir) if "222_Exp_Stenciled" in i
         ]
 
+        previous_bulk = None
         for old_name, defect_entry in self.Se_intrinsic_thermo.defect_entries.items():
             name = old_name
             for key, val in self.Se_old_new_names_dict.items():
@@ -159,21 +160,39 @@ class DefectStencilingTest(unittest.TestCase):
                 reference_struct = Structure.from_file(
                     f"{self.Se_example_dir}/{name}_222_Exp_Stenciled_POSCAR"
                 )
-                expanded_defect_supercell = get_defect_in_supercell(
-                    defect_entry,
-                    self.Se_222_expanded_supercell,
-                )
+                with warnings.catch_warnings(record=True) as w:
+                    expanded_defect_supercell, corresponding_bulk = get_defect_in_supercell(
+                        defect_entry,
+                        self.Se_222_expanded_supercell,
+                    )
+                print([str(warning.message) for warning in w])  # for debugging
+                assert not any("Note that the atomic position" in str(warning.message) for warning in w)
+
+                _check_min_dist(
+                    expanded_defect_supercell,
+                    orig_min_dist=min_dist(defect_entry.defect_supercell),
+                    warning=False,
+                )  # check min_dist, error if minimum distance has changed for some reason
+
                 expanded_defect_supercell.to(  # for debugging, removed later if tests pass
                     fmt="vasp", filename=f"{self.Se_example_dir}/{name}_gen_222_exp_POSCAR"
                 )
 
-                assert self.sm.fit(reference_struct, expanded_defect_supercell)
+                assert self.tight_sm.fit(reference_struct, expanded_defect_supercell)
                 # also directly compare structures, but note that this may change with updates to the
                 # code (and still be fine, there may be multiple quasi-equivalent stenciling choices
                 # depending on site selection around the boundary region); so mainly just for tracking
                 # changes which affect the final outputs:
-                assert expanded_defect_supercell == reference_struct
-
+                # compare Structures from POSCARs to compare like for like:
+                expanded_defect_supercell_from_poscar = Structure.from_file(
+                    f"{self.Se_example_dir}/{name}_gen_222_exp_POSCAR"
+                )
+                assert expanded_defect_supercell_from_poscar == reference_struct
                 if_present_rm(
                     f"{self.Se_example_dir}/{name}_gen_222_exp_POSCAR"
                 )  # tests passed, can remove
+
+                if previous_bulk is not None:  # check same bulk structure output in each case here
+                    assert previous_bulk == corresponding_bulk
+
+                assert self.tight_sm.fit(self.Se_222_expanded_supercell, corresponding_bulk)
