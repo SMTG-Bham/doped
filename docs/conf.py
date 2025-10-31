cba8a44af54910542fae37124ec6698506d5c645
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2025-10-30 23:42:51 -0400

    Remove unnecessary docs settings

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "JOSS"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "JOSS", "Dev_ToDo.md", "Future_ToDo.md"]
 
 myst_enable_extensions = [
     "html_admonition",
@@ -119,18 +119,10 @@ nb_execution_mode = "off"
 # nb_render_image_options = {"height": "300",}  # Reduce plots size
 # myst_render_markdown_format = "gfm"
 myst_heading_anchors = 2
-github_doc_root = "https://github.com/executablebooks/MyST-Parser/tree/master/docs/"
 
 
 def setup(app):
-    app.add_config_value(
-        "myst_parser_config",
-        {
-            "url_resolver": lambda url: github_doc_root + url,
-            "auto_toc_tree_section": "Contents",
-        },
-        True,
-    )
+    app.add_config_value("myst_parser_config", {"auto_toc_tree_section": "Contents"}, True)
     app.add_transform(AutoStructify)
 
 
